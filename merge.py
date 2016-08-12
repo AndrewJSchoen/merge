dca58f2681ebb38ba27a124ea1768214ebbdc698
AndrewJSchoen <schoen.andrewj@gmail.com>
2016-08-12 15:15:07 -0500

    Initial Commit
    [1] A basic utility for merging tables.

---
+++ merge.py
@@ -0,0 +1,78 @@
+#!/usr/bin python
+import os, sys, pandas
+from docopt import docopt
+
+Version = "1.0"
+
+doc = """
+Merge, Version {0}.
+
+Usage:
+    merge [options] (--in <FILE>.. | -i <FILE>..) [ --out <FILE> | -o <FILE>] [--by <COLUMN>...]
+
+Options:
+    -h --help                    Show this screen.
+    -v --version                 Show version.
+    --index <COLUMN>             For wide format, the column that forms the rows. [default: None]
+    --by <COLUMN>...             For wide format, the columns to merge by. [default: None]
+    --in <FILE> -i <FILE>        Input File (path).
+    --out <FILE> -o <FILE>       Output File (path) [default: ./Assembled_Values.csv].
+    --style <STYLE>              Output style, either LONG or WIDE [default: LONG]
+""".format(Version)
+
+#============================================================================
+#             General Utility
+#============================================================================
+
+def clean_path(path):
+  if path.endswith("/"):
+    path = path[:-1]
+  if path.startswith("="):
+    path = path[1:]
+  realpath = os.path.realpath(path)
+  realpath = os.path.expanduser(realpath)
+  return realpath
+
+#============================================================================
+#             Processing
+#============================================================================
+
+def run(rawargs):
+    arguments = docopt(doc, argv=rawargs, version='Merge v{0}'.format(Version))
+    arguments["--style"] = arguments["--style"].upper()
+    if arguments["--style"] not in ["LONG", "WIDE"]:
+        print("Error: '{0}' is not a valid style for output!".format(arguments["--style"]))
+        sys.exit(1)
+
+    itemarraylist = []
+    for item in arguments["--in"]:
+        try:
+            itemarraylist.append(pandas.read_csv(clean_path(item), low_memory=False, converters={arguments["--index"]: str}))
+        except:
+            print("Could not locate {0}. Ensure that your path is correct.")
+            sys.exit(1)
+    if arguments["--style"] == "LONG":
+        print("Saving file in format.")
+        outputarray = pandas.concat(itemarraylist)
+    else:
+        print("Output file as wide format.")
+        longarray = pandas.concat(itemarraylist)
+        allcolumns = [arguments["--index"]] + arguments["--by"]
+        intermediatearray = longarray.set_index(allcolumns).unstack(arguments["--by"])
+        headers_unmerged = intermediatearray.columns.values
+        headers_merged = []
+        for listitem in headers_unmerged:
+            headers_merged.append("_".join(list(listitem)[1:]))
+        intermediatearray.columns = headers_merged
+        intermediatearray[arguments["--index"]] = intermediatearray.index
+        headers_reordered = [arguments["--index"]] + headers_merged
+        intermediatearray = intermediatearray[headers_reordered]
+        outputarray = intermediatearray
+
+    outputarray.to_csv(arguments["--out"], index=False)
+
+if __name__ == '__main__':
+    args = sys.argv
+    del args[0]
+    run(args)
+    exit(0)
